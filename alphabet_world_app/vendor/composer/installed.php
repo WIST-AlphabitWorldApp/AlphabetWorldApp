cb4fd9d929b7e3d3372ff4a4e2221f05f5246bd3
EnasHameed <alqaadienas@gmail.com>
2024-03-19 14:21:59 +0300

    changed laravel.log & installed.php

--- alphabet_world_app/vendor/composer/installed.php
+++ alphabet_world_app/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'e82c95a8abfb6230eb918db94535a9aa354d0ada',
+        'reference' => 'd35ce85ddcb4cab5390feab01423d9db6c872ab6',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -382,7 +382,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'e82c95a8abfb6230eb918db94535a9aa354d0ada',
+            'reference' => 'd35ce85ddcb4cab5390feab01423d9db6c872ab6',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
